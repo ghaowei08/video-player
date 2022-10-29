fc44b8a6d47f1a06334eea908ea5f6229841d424
Haowei Goh <43960083+ghaowei08@users.noreply.github.com>
2022-10-29 08:47:47 +0800

    Embed Video

--- app/client/src/views/ContentView.vue
+++ app/client/src/views/ContentView.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    asd
+  <div class="q-ma-md">
+    <video-player :src="`http://localhost:3001/video`" controls :loop="true" :volume="0.6" @ended="handleEnded" />
   </div>
 </template>
+
+<script lang="ts" setup>
+import { VideoPlayer } from '@videojs-player/vue'
+import 'video.js/dist/video-js.css'
+
+function handleEnded() {
+  console.log("Herer")
+}
+</script>
--- server.ts
+++ server.ts
@@ -1,12 +1,12 @@
 import express, { Express, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import { createServer } from 'http'
 import dotenv from 'dotenv';
-import { log } from './utils/config';
-import { initialConnection } from './utils/mysql';
+import { createServer } from 'http'
 import { join } from 'path';
 import { expressjwt } from 'express-jwt'
+import { log } from './utils/config';
+
 import fs from 'fs'
 
 dotenv.config();
@@ -16,22 +16,24 @@ const port = process.env.APP_PORT
 app.use(cors());
 app.use(bodyParser.json())
 
-app.use(express.static(join(__dirname, '../app/client/dist'))).use(
-    expressjwt({
-        secret: process.env.SECRET_KEY!,
-        algorithms: ["HS256"],
-    }).unless({
-        path: [
-        ]
-    })
-).use((err: any, req: Request, res: Response, next: NextFunction) => {
-    log.info(`${req.method} ${req.path} `)
-    if (err.name === "UnauthorizedError") {
-        res.status(err.status).send("Unauthorized access!");
-        return;
-    }
-    next();
-});
+app.use(express.static(join(__dirname, '../app/client/dist')))
+// .use(
+//     expressjwt({
+//         secret: process.env.SECRET_KEY!,
+//         algorithms: ["HS256"],
+//     }).unless({
+//         path: [
+//             "/video"
+//         ]
+//     })
+// ).use((err: any, req: Request, res: Response, next: NextFunction) => {
+//     log.info(`${req.method} ${req.path} `)
+//     if (err.name === "UnauthorizedError") {
+//         res.status(err.status).send("Unauthorized access!");
+//         return;
+//     }
+//     next();
+// });
 
 app.get('/', async (res: Response) => {
     res.sendFile(join(__dirname, '../app/client/dist/index.html'));
@@ -41,29 +43,43 @@ app.get('/admin', async (res: Response) => {
     res.sendFile(join(__dirname, '../app/admin/dist/index.html'));
 })
 
-app.get("/video", function (req, res) {
-    const range = req.headers.range;
-    if (!range) {
-        res.status(400).send("Requires Range header");
+app.get('/test', (req, res) => {
+    const data = fs.readFileSync(join(__dirname, '../video.json'))
+    res.send(data)
+})
+
+app.get("/video", async (req, res) => {
+    const videoRange = req.headers.range;
+    const videoPath = join(__dirname, "../assets/Doritos Loaded TV Commercial - 30 second.mp4")
+    const videoSize = fs.statSync(videoPath).size;
+
+    if (videoRange) {
+        const parts = videoRange.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : videoSize - 1;
+        const chunksize = (end - start) + 1;
+        const file = fs.createReadStream(videoPath, { start, end });
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(206, head);
+        file.pipe(res);
     }
-    const videoPath = "Doritos Loaded TV Commercial - 30 second.mp4";
-    const videoSize = fs.statSync("Doritos Loaded TV Commercial - 30 second.mp4").size;
-    const CHUNK_SIZE = 10 ** 6;
-    const start = Number(range?.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-    const contentLength = end - start + 1;
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4",
-    };
-    res.writeHead(206, headers);
-    const videoStream = fs.createReadStream(videoPath, { start, end });
-    videoStream.pipe(res);
+    else {
+        const head = {
+            'Content-Length': videoSize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(videoPath).pipe(res);
+    }
 });
 
-createServer(app).listen(port, () => {
-    // initialConnection()
+createServer(app).listen(port, async () => {
     console.log(`Server listening on port http://localhost:${port}`)
 });
